710c8e3bd10c1045149403b830995df569548848
karandeep4423 <karanhanju9696@gmail.com>
2023-11-05 22:50:14 +0100

    gave correct page

--- components/Navbar.js
+++ components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
           <Link onClick={handleShow} href="/about-us">About us</Link>
           <Link onClick={handleShow} href="/contact-us">Contact us</Link>
           <div className="hidden lg:flex space-x-3">
-            <Link href="/Booking">
+            <Link href="/choose-your-lesson">
               <button className=" font-medium px-4 py-3 rounded-xl text-center overflow-hidden group bg-sky-500 relative hover:bg-gradient-to-r hover:from-sky-500 hover:to-sky-500 text-white hover:ring-2 hover:ring-offset-2 hover:ring-sky-400 transition-all ease-out duration-300">
                 <span className="absolute right-0 w-8 h-28 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
                 Get Started
